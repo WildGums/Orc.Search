72b6cb1d5c332949d8d1cedc263f1f1fb988b2ae
Geert van Horrik <geert@catenalogic.com>
2015-02-05 18:28:04 +0100

    First implementation of solution files

---
+++ src/Orc.Search.Example/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Orc.Search.Example.ViewModels
+{
+    using Catel.MVVM;
+
+    /// <summary>
+    /// MainWindow view model.
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainViewModel"/> class.
+        /// </summary>
+        public MainViewModel()
+        {
+        }
+
+        /// <summary>
+        /// Gets the title of the view model.
+        /// </summary>
+        /// <value>The title.</value>
+        public override string Title { get { return "Orc.Search.Example"; } }
+    }
+}
