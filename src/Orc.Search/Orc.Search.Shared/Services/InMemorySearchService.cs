9df23d87a4f13febe222161d937e2939cdb05af8
Geert van Horrik <geert@catenalogic.com>
2016-02-05 18:18:00 +0100

    Solution maintenance

--- src/Orc.Search/Orc.Search.Shared/Services/InMemorySearchService.cs
+++ src/Orc.Search/Orc.Search.Shared/Services/InMemorySearchService.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="InMemorySearchService.cs" company="Wild Gums">
-//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// <copyright file="InMemorySearchService.cs" company="WildGums">
+//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
--- src/Orc.Search/Orc.Search.Shared/Services/Interfaces/ISearchQueryService.cs
+++ src/Orc.Search/Orc.Search.Shared/Services/Interfaces/ISearchQueryService.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ISearchQueryService.cs" company="Wild Gums">
-//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// <copyright file="ISearchQueryService.cs" company="WildGums">
+//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
