f6386de3e2da07039508c95e1caffc60f01ee8a5
Geert van Horrik <geert@catenalogic.com>
2018-04-16 16:51:30 +0200

    Maintenance

--- src/Orc.Search.Tests.Shared/Orc.Search.Xaml.approved.cs
+++ src/Orc.Search.Tests.Shared/Orc.Search.Xaml.approved.cs
@@ -1,5 +1,7 @@
 ﻿[assembly: System.Resources.NeutralResourcesLanguageAttribute("en-US")]
 [assembly: System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.6", FrameworkDisplayName=".NET Framework 4.6")]
+[assembly: System.Windows.Markup.XmlnsDefinitionAttribute("http://schemas.wildgums.com/orc/search", "Orc.Search")]
+[assembly: System.Windows.Markup.XmlnsPrefixAttribute("http://schemas.wildgums.com/orc/search", "orcsearch")]
 public class static ModuleInitializer
 {
     public static void Initialize() { }
--- src/Orc.Search.Xaml/Properties/AssemblyInfo.cs
+++ src/Orc.Search.Xaml/Properties/AssemblyInfo.cs
@@ -7,6 +7,7 @@
 
 using System.Reflection;
 using System.Resources;
+using System.Windows.Markup;
 
 // All other assembly info is defined in SolutionAssemblyInfo.cs
 
@@ -14,3 +15,13 @@ using System.Resources;
 [assembly: AssemblyProduct("Orc.Search.Xaml")]
 [assembly: AssemblyDescription("Orc.Search.Xaml library")]
 [assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: XmlnsPrefix("http://schemas.wildgums.com/orc/search", "orcsearch")]
+[assembly: XmlnsDefinition("http://schemas.wildgums.com/orc/search", "Orc.Search")]
+//[assembly: XmlnsDefinition("http://schemas.wildgums.com/orc/search", "Orc.Search.Behaviors")]
+//[assembly: XmlnsDefinition("http://schemas.wildgums.com/orc/search", "Orc.Search.Controls")]
+//[assembly: XmlnsDefinition("http://schemas.wildgums.com/orc/search", "Orc.Search.Converters")]
+//[assembly: XmlnsDefinition("http://schemas.wildgums.com/orc/search", "Orc.Search.Fonts")]
+//[assembly: XmlnsDefinition("http://schemas.wildgums.com/orc/search", "Orc.Search.Markup")]
+//[assembly: XmlnsDefinition("http://schemas.wildgums.com/orc/search", "Orc.Search.Views")]
+//[assembly: XmlnsDefinition("http://schemas.wildgums.com/orc/search", "Orc.Search.Windows")]
